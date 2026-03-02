4801260b2f20c83ba7a35752229e5d2a74ae5ea7
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-02 13:51:12 -0600

    fix http open api issue

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,49 @@
 import { defineBackend } from "@aws-amplify/backend";
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { rdsQuery } from "./functions/rds-query/resource";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   rdsQuery,
+});
+
+const apiStack = backend.createStack("api-stack");
+
+const integration = new HttpLambdaIntegration(
+  "RdsQueryIntegration",
+  backend.rdsQuery.resources.lambda
+);
+
+const httpApi = new HttpApi(apiStack, "HttpApi", {
+  apiName: "cpcHttpApi",
+  corsPreflight: {
+    allowOrigins: ["*"],
+    allowHeaders: ["*"],
+    allowMethods: [CorsHttpMethod.POST, CorsHttpMethod.OPTIONS],
+  },
+  createDefaultStage: true,
+});
+
+httpApi.addRoutes({
+  path: "/query",
+  methods: [HttpMethod.POST, HttpMethod.OPTIONS],
+  integration,
+});
+
+// Output the URL so you can copy it from amplify_outputs / console
+backend.addOutput({
+  custom: {
+    cpcHttpApi: {
+      url: httpApi.url,
+    },
+  },
 });
