9a78c4897a41a9009a8bd722a535758e40cf7d57
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-02 12:14:27 -0600

    fixed api gateway issue

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,30 +1,10 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { rdsQuery } from "./functions/rds-query/resource";
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   rdsQuery,
 });
-
-// Create a separate stack for the REST API
-const apiStack = backend.createStack("api-stack");
-
-// REST API with open CORS (fastest for now)
-const restApi = new RestApi(apiStack, "rdsRestApi", {
-  restApiName: "rdsRestApi",
-  defaultCorsPreflightOptions: {
-    allowOrigins: Cors.ALL_ORIGINS,
-    allowMethods: Cors.ALL_METHODS,
-    allowHeaders: Cors.DEFAULT_HEADERS,
-  },
-});
-
-// Wire POST /query -> Lambda
-const queryIntegration = new LambdaIntegration(backend.rdsQuery.resources.lambda);
-const query = restApi.root.addResource("query");
-query.addMethod("POST", queryIntegration);
