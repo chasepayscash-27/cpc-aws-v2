fa3d07fe9d314bc1491ebf1368da0fcd12dce456
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-02 16:37:45 -0600

    last change

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { Amplify } from "aws-amplify";
-import outputs from "amplify_outputs";
+import outputs from "../amplify_outputs.json";
+import App from "./App";
 
 Amplify.configure(outputs);
 
