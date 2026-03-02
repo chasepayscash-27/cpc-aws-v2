c3b39af0beee3bdc95a738ff98b76083a454c4d2
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-02 16:30:10 -0600

    import outputs correction

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Amplify } from "aws-amplify";
-import outputs from "../../amplify_outputs.json";
+import outputs from "amplify_outputs";
 
 Amplify.configure(outputs);
 
