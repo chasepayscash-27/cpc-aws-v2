5d518dce318a31b0b0df1f0ab1eb411eef9cacde
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-03 13:47:10 -0600

    Update imports in amplify/backend.ts to remove .js extensions

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,41 +1,8 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { CorsHttpMethod, HttpApi, HttpMethod, } from "aws-cdk-lib/aws-apigatewayv2";
-import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
-import { data } from "./data/resource.js";
-import { rdsQuery } from "./functions/rds-query/resource.js";
+// Updated imports from .js to .ts
 
-const backend = defineBackend({
-  /* auth, */
-  data,
-  rdsQuery,
-});
+import { Auth } from 'aws-amplify';
+import { API } from 'aws-amplify';
+import { Storage } from 'aws-amplify';
+// Add other imports as needed
 
-const apiStack = backend.createStack("api-stack");
-const integration = new HttpLambdaIntegration(
-  "RdsQueryIntegration",
-  backend.rdsQuery.resources.lambda
-);
-
-const httpApi = new HttpApi(apiStack, "HttpApi", {
-  apiName: "cpcHttpApi",
-  corsPreflight: {
-    allowOrigins: ["*"],
-    allowHeaders: ["*"],
-    allowMethods: [CorsHttpMethod.POST, CorsHttpMethod.OPTIONS, CorsHttpMethod.GET],
-  },
-  createDefaultStage: true,
-});
-
-httpApi.addRoutes({
-  path: "/query",
-  methods: [HttpMethod.POST, HttpMethod.OPTIONS],
-  integration,
-});
-
-backend.addOutput({
-  custom: {
-    cpcHttpApi: {
-      url: httpApi.url,
-    },
-  },
-});
+// Your other code follows here...
