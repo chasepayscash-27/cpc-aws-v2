7300ab8ff819017e4f8ca33ddb7b1d88a83f4c81
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-04 09:53:54 -0600

    Fix formatting issue in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../amplify/data/resource";
 
-const client = generateClient<Schema>();
+const client = generateClient<Schema>(); 
 
 export default function App() {
   const [todos, setTodos] = useState<any[]>([]);
