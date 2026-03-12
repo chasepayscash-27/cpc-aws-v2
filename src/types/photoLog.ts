c8b6547af0dfe609f12692aa85c57b8808294d16
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-12 12:18:31 -0500

    update

---
+++ src/types/photoLog.ts
@@ -0,0 +1,9 @@
+export interface PhotoLogRow {
+  project_name?: string;
+  house?: string;
+  photo_date?: string;
+  uploaded_at?: string;
+  category?: string;
+  description?: string;
+  photo_url?: string;
+}
