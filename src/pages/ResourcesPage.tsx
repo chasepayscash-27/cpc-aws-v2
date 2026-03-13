5b9b0a9661bee99f582b1606e0c72ef845312714
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-13 13:20:07 -0500

    Update resource links on ResourcesPage
    Replaced existing resource links with new links for selling houses in various locations.

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const ResourcesPage: React.FC = () => {
   const links = [
-    { name: "GitHub", url: "https://github.com" },
-    { name: "React", url: "https://reactjs.org" },
-    { name: "TypeScript", url: "https://www.typescriptlang.org" },
+     { name: "Sell My House Fast In Clay", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-clay/" },
+     { name: "Sell My House Fast In Irondale", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-irondale/" },
+     { name: "Sell My House Fast In Leeds", url: " https://www.chasepayscashforhouses.com/sell-my-house-fast-in-leeds/" },
+     { name: "Sell My House Fast In Moody", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-moody-2/" },
+     { name: "Sell My House Fast In Alabaster", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-alabaster-al/" },
+     { name: "Sell My House Fast In Calera", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-calera-al/" },
+     { name: "Sell My House Fast In Chelsea", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-chelsea-al/" },
+     { name: "Sell My House Fast In Gardendale", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-gardendale-al/" },
+     { name: "Sell My House Fast In Huntsville", url: "https://www.chasepayscashforhouses.com/sell-my-house-fast-in-huntsville-al/" },
+     { name: "Sell My House Fast In Center Point", url: "https://www.chasepayscashforhouses.com/we-buy-houses-center-point/" },
+     { name: "Sell My House Fast In East Lake", url: "https://www.chasepayscashforhouses.com/we-buy-houses-east-lake/" },
+     { name: "Sell My House Fast In Grayson Valley", url: "https://www.chasepayscashforhouses.com/we-buy-houses-grayson-valley/" },  
     // Add more links as needed
   ];
 
