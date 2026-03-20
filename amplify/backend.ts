d7c2ff8804adfb803fe40da80cd31d54f53a197b
chasepayscash-27 <ahenderson@chasepayscash.com>
2026-03-19 19:18:58 -0500

    backend update

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,24 +1,47 @@
-import { HttpApi, CORSConfig } from '@aws-cdk/aws-apigatewayv2';
-import { LambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
-import * as lambda from '@aws-cdk/aws-lambda';
+import { defineBackend } from "@aws-amplify/backend";
+import { CorsHttpMethod, HttpApi, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 
-const api = new HttpApi(this, 'MyHttpApi', {
-  cors: CORSConfig.DEFAULT, // Update this to your CORS configuration
+import { data } from "./data/resource";
+import { rdsQuery } from "./functions/rds-query/resource";
+
+const backend = defineBackend({
+  /* auth, */
+  data,
+  rdsQuery,
 });
 
-const rdsQueryFunction = new lambda.Function(this, 'RDSQueryFunction', {
-  runtime: lambda.Runtime.NODEJS_14_X,
-  handler: 'rdsQuery.handler',
-  code: lambda.Code.fromAsset('lambda'),
+const apiStack = backend.createStack("api-stack");
+
+const integration = new HttpLambdaIntegration(
+  "RdsQueryIntegration",
+  backend.rdsQuery.resources.lambda
+);
+
+const httpApi = new HttpApi(apiStack, "HttpApi", {
+  apiName: "cpcHttpApi",
+  corsPreflight: {
+    allowOrigins: ["*"],
+    allowHeaders: ["*"],
+    allowMethods: [
+      CorsHttpMethod.POST,
+      CorsHttpMethod.OPTIONS,
+      CorsHttpMethod.GET,
+    ],
+  },
+  createDefaultStage: true,
 });
 
-api.addRoutes({
-  path: '/query',
-  methods: ['POST'],
-  integration: new LambdaIntegration(rdsQueryFunction),
+httpApi.addRoutes({
+  path: "/query",
+  methods: [HttpMethod.POST, HttpMethod.OPTIONS],
+  integration,
 });
 
-new cdk.CfnOutput(this, 'ApiUrl', {
-  value: api.apiEndpoint,
-  description: 'API Gateway endpoint URL for Prod stage',
+backend.addOutput({
+  custom: {
+    cpcHttpApi: {
+      url: httpApi.url,
+    },
+  },
 });
